941611feb66d99dbf8ec3b4cd1b7dd0283045f28
Jennifer-Hanson <jennkhanson@gmail.com>
2018-10-10 22:42:45 -0400

    list of parks

--- src/Components/search-bar.js
+++ src/Components/search-bar.js
@@ -14,11 +14,23 @@ const locations = {
 */
 class SearchBar extends React.Component {
   render() {
+    const locations = [
+    {name: 'Garfield Park', location: {lat:39.735663, lng:-86.147061}},
+    {name: 'Eagle Creek', location: {lat: 39.854505, lng: -86.299588 }},
+    {name: 'Broad Ripple Park', location: {lat: 39.868618, lng: -86.133435}},
+    {name: 'Sahm Park', location: {lat: 39.91976, lng: -86.053424}},
+    {name: 'Holliday Park', location: {lat: 39.870767, lng: 86.165001}},
+    ];
     //33:39 https://drive.google.com/drive/u/0/folders/1QpvhhOn_FzgB8k7TBM1jolOXdpbmbOdb
 
     return(
 <div className="container">
     <div className = "options-box">
+    <ul>
+    {locations.map(location => (
+      <li key={location.name}>{location.name}</li>
+    ))}
+    </ul>
     </div>
   </div>
 )}
