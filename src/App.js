3af04249b7ca8c5f3c50ea56432d69cf68a9f14f
Jennifer-Hanson <jennkhanson@gmail.com>
2018-10-13 21:13:32 -0400

    moved locations in to app state

--- src/App.js
+++ src/App.js
@@ -9,16 +9,17 @@ import Title from './Components/title';
 import SearchResults from './Components/search-results';
 //import SquareAPI from "./API/"
 
-const locations = [
-{name: 'Garfield Park', location: {lat:39.735663, lng:-86.147061}},
-{name: 'Eagle Creek', location: {lat: 39.854505, lng: -86.299588 }},
-{name: 'Broad Ripple Park', location: {lat: 39.868618, lng: -86.133435}},
-{name: 'Sahm Park', location: {lat: 39.91976, lng: -86.053424}},
-{name: 'Holliday Park', location: {lat: 39.870767, lng: 86.165001}},
-];
-
-
 class App extends Component {
+  state ={
+    locations:[
+    {name: 'Garfield Park', location: {lat:39.735663, lng:-86.147061}},
+    {name: 'Eagle Creek', location: {lat: 39.854505, lng: -86.299588 }},
+    {name: 'Broad Ripple Park', location: {lat: 39.868618, lng: -86.133435}},
+    {name: 'Sahm Park', location: {lat: 39.91976, lng: -86.053424}},
+    {name: 'Holliday Park', location: {lat: 39.870767, lng: 86.165001}},
+    ]
+
+  }
 
   render() {
     return (
@@ -28,12 +29,12 @@ class App extends Component {
 
      <Map
       className = "map"
-      locations = {locations}
+      locations = {this.state.locations}
       />
 
       <SearchResults
       className = "options-box"
-      locations = {locations}
+      locations = {this.state.locations}
       />
       </div>
       </div>
