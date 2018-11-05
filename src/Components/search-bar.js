506aae857f2e8748226309094746bbee42f9b63b
Jennifer-Hanson <jennkhanson@gmail.com>
2018-11-04 19:23:58 -0500

    save

--- src/Components/search-bar.js
+++ src/Components/search-bar.js
@@ -1,67 +1,72 @@
 /**
-* Search Component
-*/
+ * Search Component
+ */
 
-import React from 'react'
-import SearchList from './search-list'
+import React from "react";
+import SearchList from "./search-list";
 
 class SearchBar extends React.Component {
   constructor() {
     super();
     this.state = {
-      query: ""
-    }
+      query: "",
+      searchLocations: []
+    };
   }
+  componentWillReceiveProps = () => {
+    this.setState({
+      searchLocations: this.props.venues // passed explicitly from App, used to set initial list
+    });
+  };
 
-  handleFilterVenues = () => {};
+  handleFilterVenues = value => {
+    const tempVenues = [];
+    this.props.venues.map(venue => {
+      if (venue.venue.name.toLowerCase().includes(value.toLowerCase())) {
+        tempVenues.push(venue);
+        this.props.markers.forEach(marker => {
+          if (venue.venue.name === marker.title) {
+            marker.setVisible(true); // this functionality still needs work
+          } else {
+            marker.setVisible(false); // especially here
+          }
+        });
+      }
+      this.setState({
+        searchLocations: tempVenues // list is updated as changes occur
+      });
+    });
+  };
 
   handleChange = e => {
-    console.log(this.props.venues) //logs the array of two objects
-
-    //this.props.venue.location.name "should" get the name of the location;
-    //however, when typing in the search box, I get the error
-    //that location is undefined. Why is it undefined?
-    this.setState({ query: e.target.value });
-    const markers = this.props.venues.map(venue => {
-      const isMatched = this.props.venue.location.name
-      .toLowerCase()
-      .includes(e.target.value.toLowerCase());
-      const marker = this.props.markers.find(marker =>  venue === marker.title);
-      if(isMatched) { // will probably have to change this... TBD
-        marker.isVisible = true;
-      } else {
-        marker.isVisible = false;
-      }
-      return marker;
-    });
-    this.props.updateSuperState({markers})
-
+    this.setState(
+      { query: e.target.value },
+      this.handleFilterVenues(e.target.value)
+    );
   };
 
   render() {
     //33:39 https://drive.google.com/drive/u/0/folders/1QpvhhOn_FzgB8k7TBM1jolOXdpbmbOdb
-    return(
-    <div className='location-search'>
-    {/* was search-input */}
-    <div className="search-bar-container">
-    <input
-      type="search"
-      className="search-bar"
-      placeholder="Filter Parks..."
-      onChange={this.handleChange}
-    />
-    </div>
+    return (
+      <div className="location-search">
+        {/* was search-input */}
+        <div className="search-bar-container">
+          <input
+            type="search"
+            className="search-bar"
+            placeholder="Filter Parks..."
+            onChange={this.handleChange}
+          />
+        </div>
 
-    <SearchList {...this.props} listItemClick={this.props.listItemClick}/>
-      {/*<ul>
-      {this.props.locations.map((location) => (
-        <li  key ={location.venue.name} style={{listStyleType: "none"}} className='location-list'>{location.venue.name}</li>
-      ))}
-</ul>*/}
-    </div>
-
-)}
+        <SearchList
+          {...this.props}
+          locations={this.state.searchLocations} // now passed as an explicit list from search-bar to SearchList, which ensures it updates as changes occur
+          listItemClick={this.props.listItemClick}
+        />
+      </div>
+    );
+  }
 }
-//onclick={}
 
-export default SearchBar
+export default SearchBar;
