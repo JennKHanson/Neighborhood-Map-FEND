b45bf369f959cc1d6e82335df0e7010a61bf9a9a
Jennifer-Hanson <jennkhanson@gmail.com>
2018-11-06 00:29:44 -0500

    responsiveness tweaks

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ class App extends Component {
       client_secret: "IJB11IVJOLTYP01KV2AASJ0BMG53HI3ET5FKT4JH3WGMJAYP",
       query: "parks",
       near: "Indianapolis",
-      limit: 10,
+      limit: 5,
       v: "20181018"
     };
 
